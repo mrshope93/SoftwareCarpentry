57318496ebb881a750e164088797f46d81fb42d2
Nadya Fursova <nadezda.fursova@bioch.ox.ac.uk>
2018-10-17 23:16:19 +0100

    Extracted BMI calculation into a separate function

--- scripts/Calculate_BMI.R
+++ scripts/Calculate_BMI.R
@@ -10,18 +10,20 @@ height <- 1.7
 
 ####Calculate Body Mass Index (BMI)
 
-BMI <- weight / (height^2)
-
-###Get your results
-
-if (BMI < 16) {
-  print('Have a couple more chocolate bars')
-} else {
-  if (BMI >= 16 & BMI < 25) {
-    print('Keep on snacking')
+BMI <- function(weight, height) {
+  BMI <- weight / (height^2)
+  
+  ###Get your results
+  
+  if (BMI < 16) {
+    print('Have a couple more chocolate bars')
   } else {
-    if (BMI >= 25) {
-      print('Maybe skip that one chocolate bar before sleep')
+    if (BMI >= 16 & BMI < 25) {
+      print('Keep on snacking')
+    } else {
+      if (BMI >= 25) {
+        print('Maybe skip that one chocolate bar before sleep')
+      }
     }
   }
 }
