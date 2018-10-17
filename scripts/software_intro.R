991b21e52073e1d9f7e1628a4cdb519a7bfaf5f9
Nadya Fursova <nadezda.fursova@bioch.ox.ac.uk>
2018-10-17 21:52:01 +0100

    Started a new project

---
+++ scripts/software_intro.R
@@ -0,0 +1,110 @@
+##### Perform the following calculation
+
+2**3+25
+
+
+#### Use mathematical fuctions
+
+log10(10) # base-10 logarithm
+
+sqrt(4) # square root 
+
+
+###Comapring things
+
+1 == 1
+
+1 != 1
+
+##### Assign values to variables a and b
+
+a <- 1/50
+
+b <- 220
+
+###Print variables to find out their value
+
+a
+
+b
+
+print(a)
+
+
+####Reassign variables
+
+a <- 0.1
+
+a <- b
+
+a <- a * 2
+
+### Mathematical operations with variables
+
+log10(b)
+
+### Vector variables
+
+x <- 1:15
+
+#x
+
+#2^x
+
+y <- 2:16
+
+sum_of_vectors <- x+y
+
+
+##########Managing packages
+
+
+#You can see what packages are installed 
+
+installed.packages()
+
+#You can install packages
+
+install.packages("packagename")
+
+#You can update installed packages 
+
+update.packages()
+
+#You can remove a package  
+
+remove.packages("packagename")
+
+#You can make a package available for use 
+
+library(packagename)
+
+
+###########Challenges
+
+####Challenge 1
+
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+
+
+####Challenge 2
+
+#Compare mass to age
+
+
+####Challenge 3
+
+#Install packages ggplot2, plyr, gapminder
+
+###Just trying to change this file
+
+
+
+
+
+
+
+
